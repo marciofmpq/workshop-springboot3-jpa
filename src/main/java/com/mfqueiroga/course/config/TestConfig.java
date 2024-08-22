b5ec474e9a2e32f8b289201b3213a7735c5f2859
Marcio Felipe Queiroga <marcio.felipe@seap.ma.gov.br>
2024-08-21 21:34:59 -0300

    Payment, one to one Association

--- src/main/java/com/mfqueiroga/course/config/TestConfig.java
+++ src/main/java/com/mfqueiroga/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.mfqueiroga.course.entities.Category;
 import com.mfqueiroga.course.entities.Order;
 import com.mfqueiroga.course.entities.OrderItem;
+import com.mfqueiroga.course.entities.Payment;
 import com.mfqueiroga.course.entities.Product;
 import com.mfqueiroga.course.entities.User;
 import com.mfqueiroga.course.entities.enums.OrderStatus;
@@ -82,6 +83,13 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2024-08-14T21:53:08Z"),o1);
+		
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 	}
 
 }
